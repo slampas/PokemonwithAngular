1bb04625e5a471fd2b3cbbb30e3249cb1a5ed559
Baz <slampas@hotmail.fr>
2021-02-10 09:18:36 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemons';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'PokemonAngular';
+export class AppComponent implements OnInit {
+
+  title = 'la variable Title : PokemonAngular';
+  private pokemons: Pokemon[] | undefined;
+  public value: String = "";
+
+  public getValue() {
+    return this.value;
+  }
+
+  ngOnInit(): void {
+    this.pokemons = POKEMONS;
+  }
+
+
+
+  selectPokemon(pokemon: Pokemon) {
+    alert("Vous avez cliqué sur: " + pokemon.name);
+  }
+
+  onClick() {
+    console.log("clic numéro");
+  }
+
+  onKey(event: any) {
+   this.value = "Bonjour Monsieur: " +event.target.value;
+
+  }
+
 }
